5a7ba4e2cce5b6d2614c60e386443742cb172454
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-01-04 00:14:37 +0800

    Update mobile.js

--- js/core/mobile.js
+++ js/core/mobile.js
@@ -20,7 +20,7 @@ function simulateKeypress(key, type, shiftKey = false, ctrlKey = false) {
 
 let isMobile;
 function loadMobile() {
-	isMobile = true//detectMobile();
+	isMobile = detectMobile();
 	Vue.component('mobile-controls', {
 		data: () => { return {
 			isMobile,
@@ -31,25 +31,25 @@ function loadMobile() {
 		template: `<div style="position: absolute; border: 2px solid; background-color: var(--bg2); bottom: 0;" v-if="isMobile">
 			<div style="display: flex" v-if="placeData.node">
 				<button class="mobile fulltxt" style="width: 70px;"
-				@mousedown="simulateKeypress(' ', 'down')">Place</button>
+				@touchstart="simulateKeypress(' ', 'down')">Place</button>
 				<button class="mobile fulltxt" style="width: 70px;"
-				@mousedown="simulateKeypress('escape', 'down')">Stop</button>
+				@touchstart="simulateKeypress('escape', 'down')">Stop</button>
 				<button class="mobile fulltxt" style="width: 70px;"
-				@mousedown="sh = !sh; simulateKeypress('shift', sh ? 'down' : 'up')"
+				@touchstart="sh = !sh; simulateKeypress('shift', sh ? 'down' : 'up')"
 				:style="{'background-color': 'var(--bg-' + (sh*2 + 1) + ')'}">Shift</button>
 			</div>
 			<div style="display: flex">
 				<button class="mobile" style="visibility: hidden"></button>
 				<button class="mobile"
-				@mousedown="simulateKeypress('w', 'down')" @mouseup="simulateKeypress('w', 'up')" @blur="simulateKeypress('w', 'up')">W</button>
+				@touchstart="simulateKeypress('w', 'down')" @touchend="simulateKeypress('w', 'up')" @blur="simulateKeypress('w', 'up')">W</button>
 			</div>
 			<div style="display: flex">
 				<button class="mobile"
-				@mousedown="simulateKeypress('a', 'down')" @mouseup="simulateKeypress('a', 'up')" @blur="simulateKeypress('a', 'up')">A</button>
+				@touchstart="simulateKeypress('a', 'down')" @touchend="simulateKeypress('a', 'up')" @blur="simulateKeypress('a', 'up')">A</button>
 				<button class="mobile"
-				@mousedown="simulateKeypress('s', 'down')" @mouseup="simulateKeypress('s', 'up')" @blur="simulateKeypress('s', 'up')">S</button>
+				@touchstart="simulateKeypress('s', 'down')" @touchend="simulateKeypress('s', 'up')" @blur="simulateKeypress('s', 'up')">S</button>
 				<button class="mobile"
-				@mousedown="simulateKeypress('d', 'down')" @mouseup="simulateKeypress('d', 'up')" @blur="simulateKeypress('d', 'up')">D</button>
+				@touchstart="simulateKeypress('d', 'down')" @touchend="simulateKeypress('d', 'up')" @blur="simulateKeypress('d', 'up')">D</button>
 			</div>
 		</div>`
 	})
